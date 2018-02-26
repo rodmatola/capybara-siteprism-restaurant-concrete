6542c0e631e75909b1bbf094247676d3085fc098
Rodrigo Matola <rodmatola@gmail.com>
2018-02-26 15:08:04 -0300

    headless mode chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,18 @@ require 'capybara/rspec'
 require 'test/unit'
 
 Capybara.register_driver :selenium do |app|
+## linha abaixo para rodar normal
     Capybara::Selenium::Driver.new(app, :browser => :chrome)
+## linhas abaixo para rodar headless
+#     Capybara::Selenium::Driver.new(app,
+#     :browser => :chrome,
+#     :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
+#       'chromeOptions' => {
+#         'args' => [ "headless", "window-size=1280x720", "disable-gpu"]
+#       }
+#     )
+#   )
+# headless até aqui
 end
   
 Capybara.default_driver = :selenium
